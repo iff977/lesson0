d8469eac2a040741b17a40e17a2792e3b13314aa
iff977 <ilgizar.shigapoff1980@gmail.com>
2024-10-27 22:15:00 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"1st programm"
+print(9**0.5*5)
+"2nd programm"
+print(9.99>9.98 and 1000!=1000.1)
+"3rd programm"
+print(2*2+2 == 2*(2+2))
+"4th programm"
+print(123.456*10)
+print(type(int(123.456%10)))
+
